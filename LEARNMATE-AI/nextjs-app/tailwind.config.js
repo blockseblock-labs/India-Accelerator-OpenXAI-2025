c61ccafd37d6cddfbd54aeac80421b42e7670752
Amitlaxman-viit <amitlaxman.22310815@viit.ac.in>
2025-08-19 12:39:40 +0530

    UI Changes

--- LEARNMATE-AI/nextjs-app/tailwind.config.js
+++ LEARNMATE-AI/nextjs-app/tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        card: "var(--card)",
+        muted: "var(--muted)",
       },
     },
   },
   plugins: [],
-} 
+}
