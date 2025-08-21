e7e28d9cab47cf1b3dd2aa77905a8cbc6b9d1c35
BigRishi <rushikesh2852005@gmail.com>
2025-08-21 23:05:47 +0530

    ui small changes
    changed placeholder and slice values for example in AI climate simulator.
    changed preferred color scheme to red and white

--- BASIC-CHAT-APP/nextjs-app/app/layout.tsx
+++ BASIC-CHAT-APP/nextjs-app/app/layout.tsx
@@ -17,8 +17,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    { media: "(prefers-color-scheme: light)", color: "red" },
+    { media: "(prefers-color-scheme: dark)", color: "white" },
   ],
 };
 
